2b57452b97efabf6c40bdb5ea182a385983d7f87
manucordova <manucordova@bluewin.ch>
2022-01-17 16:53:46 +0100

    New submit script

---
+++ 1D/scripts/multi_gpu_izar_submit.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#SBATCH -J PIPNet_Train
+#SBATCH --time=3-00:00:00
+#SBATCH --nodes=1
+#SBATCH --mem 180G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:2
+#SBATCH -A lrm
+
+module purge
+module load intel intel-mpi intel-mkl
+
+source activate S2C_gpu
+
+srun python -u $1 > ${1%.py}.log
